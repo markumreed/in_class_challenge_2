106b8994d16116c9d18e5fa5d3209e6f0abd6f05
Markum Reed <markumreed@gmail.com>
2025-02-11 16:06:14 -0500

    Complete task 1

--- cc_example.js
+++ cc_example.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+// Task 1
+
+let products = [
+    { name: "Laptop", price: 1200, category: "Electronics" },// T
+    { name: "Phone", price: 800, category: "Electronics" },//T
+    { name: "Desk Chair", price: 150, category: "Furniture" },//F
+    { name: "Notebook", price: 5, category: "Stationery" },//F
+    { name: "Headphones", price: 100, category: "Electronics" }//T
+];
+
+
+// function getProductsByCategory(product_array, category_name) {
+//     return product_array.filter(product => product.category === category_name);
+// }
+
+// console.log(getProductsByCategory(products, "Electronics"));
+
+// Task 2

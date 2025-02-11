dda36b2bd6826bb15a28e48bf480953e77737371
Markum Reed <markumreed@gmail.com>
2025-02-11 16:37:39 -0500

    full commit, because Dr. Reed is an idiot... and forgot

--- cc_example.js
+++ cc_example.js
@@ -9,10 +9,41 @@ let products = [
 ];
 
 
-// function getProductsByCategory(product_array, category_name) {
-//     return product_array.filter(product => product.category === category_name);
-// }
+function getProductsByCategory(product_array, category_name) {
+    return product_array.filter(product => product.category === category_name);
+}
 
-// console.log(getProductsByCategory(products, "Electronics"));
+console.log(getProductsByCategory(products, "Electronics"));
 
 // Task 2
+
+function applyDiscount(products, discountRate) {
+    return products.map(product => ({
+        ...product,
+        price: product.price - (product.price * discountRate),
+        name: product.name.toUpperCase()
+    }
+
+    ))
+}
+console.log(applyDiscount(products, .1));
+
+Task3
+
+let sales = [250, 400, 150, 900, 1200];
+
+function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale)=>total+sale,0)
+}
+
+console.log(`Total Rev. 2024: $${calculateTotalRevenue(sales)}`)
+
+// Task 4
+
+let employee = { name: "John Doe", salary: 50000, position: "Manager" };
+
+function updateSalary(employee, percentageIncrease) {
+    return employee.salary += employee.salary * percentageIncrease;
+}
+
+console.log(updateSalary(employee, .1));
